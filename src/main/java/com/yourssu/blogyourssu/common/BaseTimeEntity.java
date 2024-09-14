78207136bba5bfaf5880f604c89e7b0c401c9f34
SeokhyunRew <darkknightttt@naver.com>
2024-09-14 16:29:06 +0900

    feat: 생성, 수정 timestap 추가

---
+++ src/main/java/com/yourssu/blogyourssu/common/BaseTimeEntity.java
@@ -0,0 +1,26 @@
+package com.yourssu.blogyourssu.common;/*
+ * created by seokhyun on 2024-09-14.
+ */
+
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Setter
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class) //자동 시간 매핑
+public abstract class BaseTimeEntity {
+    @CreatedDate
+    private LocalDateTime createAt;
+
+    @LastModifiedDate
+    private LocalDateTime modifiedAt;
+
+
+}
