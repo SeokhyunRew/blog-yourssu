71b65a5ee5f56f2547cc85d882bda257fca0094a
SeokhyunRew <darkknightttt@naver.com>
2024-09-14 15:12:53 +0900

    init : first commit

---
+++ src/main/java/com/yourssu/blogyourssu/BlogYourssuApplication.java
@@ -0,0 +1,13 @@
+package com.yourssu.blogyourssu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogYourssuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogYourssuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yourssu/blogyourssu/BlogYourssuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yourssu.blogyourssu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogYourssuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
