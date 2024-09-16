174aa81bb36796ccdd32e2b74a2457ddfa191372
SeokhyunRew <darkknightttt@naver.com>
2024-09-17 02:01:22 +0900

    feat: comment service 수정

--- src/main/java/com/yourssu/blogyourssu/service/CommentService.java
+++ src/main/java/com/yourssu/blogyourssu/service/CommentService.java
@@ -5,13 +5,9 @@ package com.yourssu.blogyourssu.service;/*
 import com.yourssu.blogyourssu.domain.ArticleEntity;
 import com.yourssu.blogyourssu.domain.CommentEntity;
 import com.yourssu.blogyourssu.domain.UserEntity;
-import com.yourssu.blogyourssu.dto.request.ArticleRequest;
 import com.yourssu.blogyourssu.dto.request.CommentRequest;
-import com.yourssu.blogyourssu.dto.response.ArticleResponse;
 import com.yourssu.blogyourssu.dto.response.CommentResponse;
-import com.yourssu.blogyourssu.dto.util.ArticleDtoUtil;
 import com.yourssu.blogyourssu.dto.util.CommentDtoUtil;
-import com.yourssu.blogyourssu.reposiotry.ArticleRepository;
 import com.yourssu.blogyourssu.reposiotry.CommentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -25,19 +21,21 @@ public class CommentService {
     private final CommentRepository commentRepository;
     private final CommentSearchService commentSearchService;
     private final ArticleSearchService articleSearchService;
+    private final UserSearchService userSearchService;
 
-
-    public CommentResponse createComment(CommentRequest request, UserEntity userEntity, Long articleId){
+    public CommentResponse createComment(CommentRequest request, Long userId, Long articleId){
         ArticleEntity findArticle = articleSearchService.findById(articleId);
-        CommentEntity saveComment = commentRepository.save(request.toEntity(findArticle, userEntity));
+        UserEntity findUser = userSearchService.findById(userId);
+
+        CommentEntity saveComment = commentRepository.save(request.toEntity(findArticle, findUser));
 
         return CommentDtoUtil.commentToCommentResponse(saveComment) ;
     }
 
-    public CommentResponse updateComment(Long commentId, UserEntity userEntity, CommentRequest request){
+    public CommentResponse updateComment(Long commentId, Long userId, CommentRequest request){
         CommentEntity findComment = commentSearchService.findById(commentId);
 
-        if (!findComment.getUserEntity().getId().equals(userEntity.getId())) {
+        if (!findComment.getUserEntity().getId().equals(userId)) {
             throw new IllegalArgumentException("작성자만 수정 가능합니다.");
         }
 
@@ -46,10 +44,10 @@ public class CommentService {
         return CommentDtoUtil.commentToCommentResponse(findComment);
     }
 
-    public void deleteById(Long commentId, UserEntity userEntity){
+    public void deleteById(Long commentId, Long userId){
         CommentEntity findComment = commentSearchService.findById(commentId);
 
-        if (!findComment.getUserEntity().getId().equals(userEntity.getId())) {
+        if (!findComment.getUserEntity().getId().equals(userId)) {
             throw new IllegalArgumentException("작성자만 삭제 가능합니다.");
         }
 
