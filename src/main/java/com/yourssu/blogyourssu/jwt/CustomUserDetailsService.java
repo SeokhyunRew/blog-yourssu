ee57d8b6976aa4776767f219e3541dd377fd0971
SeokhyunRew <darkknightttt@naver.com>
2024-09-18 20:09:56 +0900

    feat: 예외처리 추가

--- src/main/java/com/yourssu/blogyourssu/jwt/CustomUserDetailsService.java
+++ src/main/java/com/yourssu/blogyourssu/jwt/CustomUserDetailsService.java
@@ -2,9 +2,12 @@ package com.yourssu.blogyourssu.jwt;/*
  * created by seokhyun on 2024-09-16.
 */
 
+import com.yourssu.blogyourssu.common.exception.customexception.NotFoundException;
 import com.yourssu.blogyourssu.domain.UserEntity;
 import com.yourssu.blogyourssu.reposiotry.UserRepository;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -20,7 +23,9 @@ public class CustomUserDetailsService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
         //DB에서 조회
-        UserEntity userData = userRepository.findByEmail(username);
+        UserEntity userData = Optional.ofNullable(userRepository.findByEmail(username))
+                .orElseThrow(() -> new NotFoundException("사용자를 찾을 수 없습니다.") {
+                });
 
         if (userData != null) {
 
